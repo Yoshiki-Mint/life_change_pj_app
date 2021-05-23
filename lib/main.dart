9fd9b8d019e9f68c5b0e317aed1a2098dfe1bd50
Mint Mitsurugi <kg-ysk@orchid.plala.or.jp>
2021-05-23 21:18:21 +0900

    fire_base setting problem is solved!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:life_change_pj_app/next_page.dart';
 import 'package:table_calendar/table_calendar.dart';
-//import 'dart:developer';
+// import 'dart:developer';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
